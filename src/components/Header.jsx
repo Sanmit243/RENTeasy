1f1b2a43fb22181f13e0a22791277e1247617aa3
Sanmit243 <243sanmitpatole@gmail.com>
2024-08-08 16:23:12 +0530

    Created Header Component (i.e. Navbar)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,43 @@
 import React from 'react'
+import {FaSearch} from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <header className='bg-purple-100 shadow-md'>
+      <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+      
+      {/* icon */}
+      <Link to="/">
+      <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
+          <span className='text-purple-600'>RENT</span>
+          <span className='text-purple-900'>easy</span>
+      </h1>
+      </Link>
+
+      {/* search bar */}
+      <form className='bg-purple-50 p-3 rounded-lg flex items-center'>
+        <input type="text" placeholder='Search' className='bg-transparent focus:outline-none w-24 sm:w-64'/>
+        <FaSearch className='text-purple-600'/>
+      </form>
+
+      {/* menu */}
+      <ul className='flex gap-4 '>
+        <Link to="/">
+        <li className='hidden sm:inline text-purple-700 hover:underline'>
+          Home
+        </li>
+        </Link>
+        <Link to="/about">
+        <li className='hidden sm:inline text-purple-700 hover:underline'>
+        About
+        </li>
+        </Link>
+        <Link to="/sign-in">
+        <li className='text-purple-700 hover:underline'>Sign in</li>
+        </Link>
+      </ul>
+      </div>
+      </header>
   )
 }
